bf36fb73399e5ca7c74cb36542468791ae367d31
Antony Blakey <antony@soops.nl>
2019-04-06 18:37:36 +0200

    Small selector config refactoring

--- src/window_manager.rs
+++ src/window_manager.rs
@@ -22,28 +22,36 @@ pub struct WindowSelectorArtist {
     windows: Vec<xcb::Window>,
 }
 
+struct Point {
+    x: u16,
+    y: u16,
+}
+
 impl WindowSelectorArtist {
-    const MARGIN: (u16, u16) = (6, 2);
-    const LABEL_PADDING: (u16, u16) = (4, 2);
+    const FONT_FACE: &'static str = "Noto Sans Mono";
+    const FONT_SIZE: u16 = 12;
+
+    const MARGIN: Point = Point { x: 6, y: 2 };
+    const LABEL_PADDING: Point = Point { x: 4, y: 2 };
     const LABEL_TO_NAME_GAP: u16 = 6;
     const LINE_SPACING: u16 = 2;
 
     fn configure_label_font(&self, context: &cairo::Context) {
         context.select_font_face(
-            "Ubuntu Mono",
+            Self::FONT_FACE,
             cairo::FontSlant::Normal,
             cairo::FontWeight::Bold,
         );
-        context.set_font_size(14.0);
+        context.set_font_size(Self::FONT_SIZE as f64);
     }
 
     fn configure_name_font(&self, context: &cairo::Context) {
         context.select_font_face(
-            "Ubuntu Mono",
+            Self::FONT_FACE,
             cairo::FontSlant::Normal,
             cairo::FontWeight::Normal,
         );
-        context.set_font_size(14.0);
+        context.set_font_size(Self::FONT_SIZE as f64);
     }
 }
 
@@ -53,11 +61,11 @@ impl artist::Artist for WindowSelectorArtist {
             Ok(geometry) => {
                 if let Ok(surface) = get_cairo_surface(window) {
                     let context = cairo::Context::new(&surface);
-                    
+
                     self.configure_label_font(&context);
                     let font_extents = context.font_extents();
                     let line_height = font_extents.height.ceil() as u16;
-                    
+
                     let mut label_width = 0;
                     for label in &self.labels {
                         let text_extents = context.text_extents(label);
@@ -75,21 +83,21 @@ impl artist::Artist for WindowSelectorArtist {
                     return Some(euclid::rect(
                         geometry.x() as u16,
                         geometry.y() as u16,
-                        Self::MARGIN.0
-                            + Self::LABEL_PADDING.0
+                        Self::MARGIN.x
+                            + Self::LABEL_PADDING.x
                             + label_width
-                            + Self::LABEL_PADDING.0
+                            + Self::LABEL_PADDING.x
                             + Self::LABEL_TO_NAME_GAP
                             + name_width
-                            + Self::MARGIN.0,
-                        Self::MARGIN.1
+                            + Self::MARGIN.x,
+                        Self::MARGIN.y
                             + self.labels.len() as u16
-                                * (Self::LABEL_PADDING.1
+                                * (Self::LABEL_PADDING.y
                                     + line_height
-                                    + Self::LABEL_PADDING.1
+                                    + Self::LABEL_PADDING.y
                                     + Self::LINE_SPACING)
                             - Self::LINE_SPACING
-                            + Self::MARGIN.1,
+                            + Self::MARGIN.y,
                     ));
                 }
             }
@@ -115,11 +123,11 @@ impl artist::Artist for WindowSelectorArtist {
             }
 
             {
-                let mut top = Self::MARGIN.1;
-                let left = Self::MARGIN.0;
-                let right = left + Self::LABEL_PADDING.0 + label_width + Self::LABEL_PADDING.0;
+                let mut top = Self::MARGIN.y;
+                let left = Self::MARGIN.x;
+                let right = left + Self::LABEL_PADDING.x + label_width + Self::LABEL_PADDING.x;
                 for label in &self.labels {
-                    let bottom = top + Self::LABEL_PADDING.1 + line_height + Self::LABEL_PADDING.1;
+                    let bottom = top + Self::LABEL_PADDING.y + line_height + Self::LABEL_PADDING.y;
 
                     context.set_source_rgb(0.4, 0.0, 0.0);
                     context.move_to(left as f64, top as f64);
@@ -131,8 +139,8 @@ impl artist::Artist for WindowSelectorArtist {
 
                     context.set_source_rgb(1.0, 1.0, 1.0);
                     context.move_to(
-                        (left + Self::LABEL_PADDING.0) as f64,
-                        (top + Self::LABEL_PADDING.1) as f64 + ascent - 1.0,
+                        (left + Self::LABEL_PADDING.x) as f64,
+                        (top + Self::LABEL_PADDING.y) as f64 + ascent,
                     );
                     context.show_text(label);
 
@@ -142,21 +150,18 @@ impl artist::Artist for WindowSelectorArtist {
 
             {
                 self.configure_name_font(&context);
-                let mut top = Self::MARGIN.1;
-                let left = Self::MARGIN.0
-                    + Self::LABEL_PADDING.0
+                let mut top = Self::MARGIN.y;
+                let left = Self::MARGIN.x
+                    + Self::LABEL_PADDING.x
                     + label_width
-                    + Self::LABEL_PADDING.0
+                    + Self::LABEL_PADDING.x
                     + Self::LABEL_TO_NAME_GAP;
                 context.set_source_rgb(0.2, 0.2, 0.5);
                 for window in &self.windows {
-                    let bottom = top + Self::LABEL_PADDING.1 + line_height + Self::LABEL_PADDING.1;
+                    let bottom = top + Self::LABEL_PADDING.y + line_height + Self::LABEL_PADDING.y;
 
                     let name = get_string_property(*window, *ATOM__NET_WM_NAME);
-                    context.move_to(
-                        left as f64,
-                        (top + Self::LABEL_PADDING.1) as f64 + ascent - 1.0,
-                    );
+                    context.move_to(left as f64, (top + Self::LABEL_PADDING.y) as f64 + ascent);
                     context.show_text(&name);
 
                     top = bottom + Self::LINE_SPACING;
